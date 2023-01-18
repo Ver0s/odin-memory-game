6125449b118b2fe0e8eb7f717be51a59be13bde4
Arek <arek.stachyra41@gmail.com>
2023-01-18 14:37:04 +0100

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer(): JSX.Element {
+	return (
+		<footer className="bg-slate-500">
+			<div className="mx-auto max-w-7xl bg-slate-500 py-4 text-center">
+				<span>xdd</span>
+			</div>
+		</footer>
+	);
+}
