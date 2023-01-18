48b2348f4159e0081b993efb763f81e20e8d1c25
Arek <arek.stachyra41@gmail.com>
2023-01-18 14:36:31 +0100

    add custom grid layout to taiwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		gridTemplateColumns: {
+			'auto-fit': 'repeat(auto-fit, minmax(180px, 1fr))',
+		},
 	},
 	plugins: [],
 };
